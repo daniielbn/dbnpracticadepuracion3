1f4ed9f8d45f669985f4222f295d588c5d52105a
Daniel <danielbritonegrin@gmail.com>
2023-11-27 20:32:23 +0000

    Práctica 3 Ejercicio 3 sin arreglar

---
+++ ejercicio3.java
@@ -0,0 +1,19 @@
+public class ejercicio3 {
+    public static void EJ3() {
+        //Creo un array
+        int[] numeros=new int[100];
+        //Declaro las variables necesarias
+        int suma=0;
+        double media;
+        //Recorro el array, asigno números y sumo
+        for(int i=1;i<numeros.length-1;i++){
+            numeros[i]=i+1;
+            suma=numeros[i];
+        }
+        //Calculo la media y muestro la suma y la meda
+        System.out.println("La suma es "+suma);
+        media=(double)suma/numeros.length-1;
+        System.out.println("La media es "+media);
+    }
+        
+}
