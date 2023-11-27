c68f6e1866136dd5603d9ae3dcf90cdd69a6f277
Daniel <danielbritonegrin@gmail.com>
2023-11-27 20:35:16 +0000

    Práctica 3 Ejercicio 3 arreglado

--- ejercicio3.java
+++ ejercicio3.java
@@ -6,13 +6,13 @@ public class ejercicio3 {
         int suma=0;
         double media;
         //Recorro el array, asigno números y sumo
-        for(int i=1;i<numeros.length-1;i++){
+        for(int i=0;i<numeros.length/*-1*/;i++){
             numeros[i]=i+1;
             suma=numeros[i];
         }
         //Calculo la media y muestro la suma y la meda
         System.out.println("La suma es "+suma);
-        media=(double)suma/numeros.length-1;
+        media=(double)suma/numeros.length/*-1*/;
         System.out.println("La media es "+media);
     }
         
