2d108cafc15a3d9196fea587b8e03f316f63495d
Daniel <danielbritonegrin@gmail.com>
2023-11-27 20:32:03 +0000

    Práctica 3 Ejercicio 1 sin arreglar

---
+++ ejercicio1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void EJ1() {
+        Scanner sn=new Scanner(System.in);
+        //Pido al usuario que escriba una frase
+        System.out.println("Escriba una frase");
+        //Recomiendo usar nextLine en lugar de next por los espacios
+        String frase=sn.nextLine();
+        //Creamos un array de caracteres usando el metodo de String
+        char caracteres[]=frase.toCharArray();
+        //Recorremos la frase y cogemos cada caracter y lo metemos en el array
+        for(int i=1;i<frase.length();i++){
+            System.out.println("En la posicion del array : " + i+1 + "tenemos el caracter : " + caracteres[i+1]);
+        }
+    }
+
+}
